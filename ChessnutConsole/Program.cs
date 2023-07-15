766b14c6b2c11fb65bf859a5144ae64825f8511f
Travus Bowline <travusb@gmail.com>
2023-07-15 10:07:10 -0600

    Add .sln file

--- ChessnutConsole/Program.cs
+++ ChessnutConsole/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using EasyLinkSDKCSharp;
+﻿using EasyLinkSDKCSharp;
 
 using ChessnutAirAPI api = new ChessnutAirAPI();
 api.Connect();
