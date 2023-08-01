08f53e7a00690094a18f633e8c4a8c2cd5bc6f27
Travus Bowline <travusb@gmail.com>
2023-07-31 19:04:10 -0600

    Name change

---
+++ ChessnutBoardEmulator/Converters/FileToGridConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ChessnutBoardEmulator.Converters;
+
+public class FileToGridConverter : IMultiValueConverter
+{
+    /*public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int file)
+        {
+            return Math.Abs(file - 1);
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }*/
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length < 2)
+            return null;
+
+        if (values[0] is int file && values[1] is bool isReversed)
+            return isReversed ? Math.Abs(file - 8) : file - 1;
+
+        return null;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
